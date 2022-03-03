81c4939926123ad2b134db2031cf3b0e384e2e00
csumn <csumn13@gmail.com>
2022-03-03 14:42:42 +0530

    no action done

--- LineComparision/src/com/bridgelabz/LineComparision.java
+++ LineComparision/src/com/bridgelabz/LineComparision.java
@@ -1,8 +1,24 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class LineComparision {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparision Program");
-	}
+		
+			Scanner obj = new Scanner(System.in);
+	        System.out.println("Enter value for x1 : ");
+	        int x1 = obj.nextInt();
+	        System.out.println("Enter value for y1 : ");
+	        int y1 = obj.nextInt();
+	        System.out.println("Enter value for x2 : ");
+	        int x2 = obj.nextInt();
+	        System.out.println("Enter value for y2 : ");
+	        int y2 = obj.nextInt();
+	        obj.close();
+
+	        double LengthOfLine = Math.sqrt( ( x2 - x1 ) * ( x2 - x1 ) + ( y2 - y1 ) * ( y2 - y1 ));
+
+	        System.out.println(" Length of Line is : "+LengthOfLine);
+	        }
 }
